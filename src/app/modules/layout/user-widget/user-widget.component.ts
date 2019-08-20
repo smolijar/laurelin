59174f7747d8d46e9cd532ff01480828d95de773
Jaroslav Šmolík <grissius@gmail.com>
2019-08-20 07:25:38 +0200

    ✨ Add user-widget component in layout

---
+++ src/app/modules/layout/user-widget/user-widget.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { SessionQuery } from 'src/app/state/session/session.query';
+import { SessionService } from 'src/app/state/session/session.service';
+
+@Component({
+  selector: 'lin-user-widget',
+  templateUrl: './user-widget.component.html',
+})
+export class UserWidgetComponent {
+  constructor(
+    private sessionService: SessionService,
+    public sessionQuery: SessionQuery
+  ) {}
+
+  signIn() {
+    this.sessionService.signIn();
+  }
+
+  signOut() {
+    this.sessionService.signOut();
+  }
+}
