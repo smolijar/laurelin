2d08feb02005707691d705b9ff0eeb456e135c12
Jaroslav Šmolík <grissius@gmail.com>
2019-08-20 07:26:11 +0200

    ✨ Add akita persistance

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { persistState, enableAkitaProdMode } from '@datorama/akita';
 
 if (environment.production) {
   enableProdMode();
+  enableAkitaProdMode();
 }
+
+persistState();
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
